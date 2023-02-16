f4ea5260239b1f066a15059e69427cd5a70bc14d
josubin47 <3_3_jimca@naver.com>
2023-02-16 21:18:04 +0900

    refactor: element return하는 함수 추가

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,17 @@
+const $ = (selector) => document.querySelector(selector);
+
 function App() {
   // form 태그 자동 전송 막기
-  document
-    .querySelector("#espresso-menu-form")
-    .addEventListener("submit", (e) => {
-      e.preventDefault();
-    });
+  $("#espresso-menu-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
 
   // 메뉴의 이름을 입력 받고 엔터키 입력으로 추가한다.
-  document
-    .querySelector("#espresso-menu-name")
-    .addEventListener("keypress", (e) => {
-      if (e.key === "Enter") {
-        document.querySelector("#espresso-menu-name").value;
-      }
-    });
+  $("#espresso-menu-name").addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+      document.querySelector("#espresso-menu-name").value;
+    }
+  });
 }
 
 App();
