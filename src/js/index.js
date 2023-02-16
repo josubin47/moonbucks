fc06b9e51e526aeb363a1df26925d0057108af4a
josubin47 <3_3_jimca@naver.com>
2023-02-16 21:10:34 +0900

    feat: form 태그 자동 전송 막기

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,11 @@
 function App() {
+  // form 태그 자동 전송 막기
+  document
+    .querySelector("#espresso-menu-form")
+    .addEventListener("submit", (e) => {
+      e.preventDefault();
+    });
+
   // 메뉴의 이름을 입력 받고 엔터키 입력으로 추가한다.
   document
     .querySelector("#espresso-menu-name")
