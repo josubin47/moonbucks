ef06f2cad38343642192996488d48528e43b0308
josubin47 <3_3_jimca@naver.com>
2023-02-16 22:19:00 +0900

    fix: 메뉴 count 안 되는 문제 수정

--- src/js/index.js
+++ src/js/index.js
@@ -1,46 +1,43 @@
 const $ = (selector) => document.querySelector(selector);
 
-const addMenu = () => {
-  let count = 0;
+function App() {
+  const addMenu = () => {
+    const $espressoMenuName = $("#espresso-menu-name").value;
 
-  const $espressoMenuName = $("#espresso-menu-name").value;
+    if ($espressoMenuName === "") {
+      alert("값을 입력해주세요!");
+      return;
+    }
 
-  if ($espressoMenuName === "") {
-    alert("값을 입력해주세요!");
-    return;
-  }
+    //입력한 값을 메뉴 리스트에 추가
+    const menuItemTemplate = (espressoMenuName) => {
+      return `<li class="menu-list-item d-flex items-center py-2">
+              <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
+              <button
+                type="button"
+                class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+              >
+                수정
+              </button>
+              <button
+                type="button"
+                class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+              >
+                삭제
+              </button>
+            </li>`;
+    };
 
-  //입력한 값을 메뉴 리스트에 추가
-  const menuItemTemplate = (espressoMenuName) => {
-    return `<li class="menu-list-item d-flex items-center py-2">
-        <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
-        <button
-          type="button"
-          class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
-        >
-          수정
-        </button>
-        <button
-          type="button"
-          class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
-        >
-          삭제
-        </button>
-      </li>`;
+    $("#espresso-menu-list").insertAdjacentHTML(
+      "beforeend",
+      menuItemTemplate($espressoMenuName)
+    );
+    const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
+    $(".menu-count").innerText = `총 ${menuCount}개`;
+
+    $("#espresso-menu-name").value = "";
   };
 
-  $("#espresso-menu-list").insertAdjacentHTML(
-    "beforeend",
-    menuItemTemplate($espressoMenuName)
-  );
-  count++;
-
-  $(".menu-count").innerText = `총 ${count}개`;
-
-  $("#espresso-menu-name").value = "";
-};
-
-function App() {
   // form 태그 자동 전송 막기
   $("#espresso-menu-form").addEventListener("submit", (e) => {
     e.preventDefault();
