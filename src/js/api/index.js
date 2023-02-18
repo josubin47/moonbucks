5fc49f23033a460ef9bf2a7d0ed6dea66cb7c69c
josubin47 <3_3_jimca@naver.com>
2023-02-19 02:37:07 +0900

    refactor: API 객체 추상화

--- src/js/api/index.js
+++ src/js/api/index.js
@@ -1,67 +1,78 @@
 const BASE_URL = "http://localhost:3000/api";
 
-const api = {
-  async getMenuList(category) {
-    const response = await fetch(`${BASE_URL}/category/${category}/menu`);
-    console.log(response);
-    return response.json();
-  },
-  async createMenu(category, name) {
-    const response = await fetch(`${BASE_URL}/category/${category}/menu`, {
+const headers = { "Content-Type": "application/json" };
+
+const HTTP_METHOD = {
+  POST(data) {
+    return {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ name }),
-    });
-    if (!response.ok) {
-      console.error("에러가 발생했습니다");
-    }
+      headers: headers,
+      body: JSON.stringify(data),
+    };
   },
-  async updateMenu(category, menuId, name) {
-    const response = await fetch(
-      `${BASE_URL}/category/${category}/menu/${menuId}`,
-      {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ name }),
-      }
+  PUT(data) {
+    return {
+      method: "PUT",
+      headers: headers,
+      body: data ? JSON.stringify(data) : null,
+    };
+  },
+  DELETE() {
+    return {
+      method: "DELETE",
+    };
+  },
+};
+
+const request = async (url, option) => {
+  const response = await fetch(url, option);
+
+  if (!response.ok) {
+    alert("에러가 발생했습니다.");
+    console.error(response);
+  }
+
+  return response.json();
+};
+
+const requestWithoutJson = async (url, option) => {
+  const response = await fetch(url, option);
+
+  if (!response.ok) {
+    alert("에러가 발생했습니다.");
+    console.error(response);
+  }
+
+  return response;
+};
+
+const api = {
+  getMenuList(category) {
+    return request(`${BASE_URL}/category/${category}/menu`);
+  },
+  createMenu(category, name) {
+    return request(
+      `${BASE_URL}/category/${category}/menu`,
+      HTTP_METHOD.POST({ name })
     );
-    if (!response.ok) {
-      console.error("에러가 발생했습니다");
-    }
-    return response.json();
   },
-  async toggleSoldOutMenu(category, menuId) {
-    const response = await fetch(
+  updateMenu(category, menuId, name) {
+    return request(
+      `${BASE_URL}/category/${category}/menu/${menuId}`,
+      HTTP_METHOD.PUT({ name })
+    );
+  },
+  toggleSoldOutMenu(category, menuId) {
+    return request(
       `${BASE_URL}/category/${category}/menu/${menuId}/soldout`,
-      {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
+      HTTP_METHOD.PUT()
     );
-    if (!response.ok) {
-      alert("");
-      console.error("에러가 발생했습니다");
-    }
   },
-  async deleteMenu(category, menuId) {
-    const response = await fetch(
+  deleteMenu(category, menuId) {
+    return requestWithoutJson(
       `${BASE_URL}/category/${category}/menu/${menuId}`,
-      {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
+      HTTP_METHOD.DELETE()
     );
-    if (!response.ok) {
-      console.error("에러가 발생했습니다");
-    }
   },
 };
 
